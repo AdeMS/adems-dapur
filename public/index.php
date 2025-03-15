030eee6c0344e82275b13ccf9578dfb805eee82a
AdeMS <ademokoginta@gmail.com>
2025-03-15 19:07:42 +0700

    Updating index.php as request starting point

--- public/index.php
+++ public/index.php
@@ -1,3 +1,22 @@
 <?php
 
 declare(strict_types=1);
+
+use AdeMS\Mvc\Application;
+
+// set start time
+define('DAPUR_START', microtime(true));
+
+// Change the working directory to application root
+chdir(dirname(__DIR__));
+
+// Composer autoloading
+include getcwd() . '/vendor/autoload.php';
+
+// Application container init..
+$container = require getcwd() . '/config/application.container.php';
+
+// Run the application!
+/** @var Application $app */
+$app = $container->get('Application');
+$app->run();
