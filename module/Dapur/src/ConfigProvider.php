03fce11e25210075a4fa1036d4c3e7228fdf327f
AdeMS <ademokoginta@gmail.com>
2026-04-30 16:03:34 +0700

    Menambahkan file kelas ConfigProvider untuk mengelola konfigurasi aplikasi Dapur

---
+++ module/Dapur/src/ConfigProvider.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dapur;
+
+class ConfigProvider
+{
+    /**
+     * Returns the configuration array.
+     *
+     * To add a bit of a structure, each section is defined in a separate
+     * file in the 'config' folder.
+     */
+    public function __invoke(): array
+    {
+        return [
+            'dependencies' => $this->getDependencies(),
+            'templates'    => $this->getTemplates(),
+        ];
+    }
+
+    /**
+     * Returns the container dependencies.
+     */
+    public function getDependencies(): array
+    {
+        return [
+            'invokables' => [
+                
+            ],
+            'factories'  => [
+
+            ],
+        ];
+    }
+
+    /**
+     * Returns the templates configuration.
+     */
+    public function getTemplates(): array
+    {
+        return [
+            'paths' => [
+                'error'  => [dirname(__DIR__) . '/templates/error'],
+            ],
+        ];
+    }
+}
