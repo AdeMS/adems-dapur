ffa6791ac213b06cdad613414136e9157523b000
AdeMS <ademokoginta@gmail.com>
2026-05-07 01:11:12 +0700

    Menambahkan konfigurasi provider Mezzio ke dalam ConfigAggregator

--- config/config.php
+++ config/config.php
@@ -5,6 +5,12 @@ declare(strict_types=1);
 use Laminas\ConfigAggregator\ConfigAggregator;
 
 $aggregator = new ConfigAggregator([
+    \Mezzio\LaminasView\ConfigProvider::class,
+    \Mezzio\Router\LaminasRouter\ConfigProvider::class,
+    \Laminas\Router\ConfigProvider::class,
+    \Laminas\HttpHandlerRunner\ConfigProvider::class,
+    \Laminas\Validator\ConfigProvider::class,
+    
     \Mezzio\Helper\ConfigProvider::class,
     \Mezzio\ConfigProvider::class,
     \Mezzio\Router\ConfigProvider::class,
